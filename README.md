112a259876e1ad51f72daf3c5495c5fd1847174e
netboy <netboy@netboy.pl>
2012-05-03 23:27:59 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,122 @@
-html2less
-=========
+html2less.py
+=============
 
-Parse HTML5 and output nested rulesets (compatible with LESS) kickstarting the work on a CSS stylesheet. 
+html2less.py takes a HTML5 document and outputs nested rulesets (compatible with [LESS](http://lesscss.org/)) kickstarting the work on a CSS stylesheet.
+
+What it does?
+-------------
+
+```
+./html2less.py --help
+
+Usage: html2less.py [options] input files
+
+Options:
+  --version             show program's version number and exit
+  -h, --help            show this help message and exit
+  -d DELIMITER, --delimiter=DELIMITER
+                        Delimiter: tabs or spaces? Default: spaces
+  -c, --clean           Give me clean rulesets (no comments)
+```
+
+How it works?
+-------------
+
+Sample HTML5 document:
+
+```
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Sample HTML5 Document</title>
+</head>
+<body>
+    <div id="container">
+        <header>
+            <h1>Sample HTML5 Structure</h1>
+            <nav class="navigation">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                </ul>
+            </nav>
+        </header>
+        <section class="page-content">
+            <hgroup>
+                <h1>Main Section</h1>
+                <h2>This is a sample HTML5 Page</h2>
+            </hgroup>
+            <article>
+                <p>This is the content for the first article</p>
+            </article>
+        </section>
+        <footer class="page-footer">
+            <p>This is the Footer</p>
+        </footer>
+    </div>
+</body>
+</html>
+```
+
+Let's extract CSS rulesets from the document:
+
+```
+./html2less.py test.html
+#container {
+  /* enter your CSS here... */
+  header {
+    /* enter your CSS here... */
+    h1 {
+      /* enter your CSS here... */
+    }
+    .navigation {
+      /* enter your CSS here... */
+      ul {
+        /* enter your CSS here... */
+        li {
+          /* enter your CSS here... */
+          a {
+            /* enter your CSS here... */
+          }
+        }
+        li {
+          /* enter your CSS here... */
+          a {
+            /* enter your CSS here... */
+          }
+        }
+      }
+    }
+  }
+  .page-content {
+    /* enter your CSS here... */
+    hgroup {
+      /* enter your CSS here... */
+      h1 {
+        /* enter your CSS here... */
+      }
+      h2 {
+        /* enter your CSS here... */
+      }
+    }
+    article {
+      /* enter your CSS here... */
+      p {
+        /* enter your CSS here... */
+      }
+    }
+  }
+  .page-footer {
+    /* enter your CSS here... */
+    p {
+      /* enter your CSS here... */
+    }
+  }
+}
+```
+
+Dependencies
+------------
+
+This script depends on [lxml](http://pypi.python.org/pypi/lxml).
---
+++ html2less.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python
+
+import os, sys
+from optparse import OptionParser
+
+try:
+    from lxml import etree
+    from lxml.html.clean import Cleaner
+except ImportError:
+    print "Please install lxml first."
+    print "http://pypi.python.org/pypi/lxml"
+    exit(1)
+
+from StringIO import StringIO
+
+class Html2Less(object):
+    """Parse HTML to extract empty CSS rulesets."""
+
+    def __init__(self):
+        self.prog = os.path.basename(sys.argv[0])
+
+        parser = OptionParser(version="%prog 0.1", usage="%prog [options] input files")
+        parser.add_option("-d", "--delimiter", dest="delimiter", default='spaces',
+            help="Delimiter: tabs or spaces? Default: spaces")
+        parser.add_option("-c", "--clean", action="store_true", dest="clean_mode", default=False,
+            help="Give me clean rulesets (no comments)")
+
+        (self.options, self.args) = parser.parse_args()
+
+        if self.options.delimiter and self.options.delimiter not in ['tabs', 'spaces']:
+            parser.error("Accepted delimiter value: 'tabs' or 'spaces'")
+
+        if not self.args:
+            parser.error("no input files specified")
+
+    def identify_ruleset(self, elem):
+        """Identify a ruleset - give precedence to: A) id, B) class, C) tag name."""
+
+        identifier = elem.attrib.get('id')
+        if identifier:
+            identifier = "#" + identifier
+        else:
+            identifier = elem.attrib.get('class')
+            if identifier:
+                identifier = "." + identifier.split()[0]
+            else:
+                identifier = elem.tag
+
+        return identifier
+
+    def parse(self, content):
+        """Clean and parse HTML content."""
+
+        cleaner = Cleaner(style=True, links=False, page_structure=False, meta=True,
+            safe_attrs_only=False, remove_unknown_tags=False)
+        clean_content = cleaner.clean_html(content)
+
+        html = etree.iterparse(StringIO(clean_content), events=("start", "end"))
+        level = -1
+        css = ''
+
+        # We do not want to style these elements.
+        ignore_tags = ['html', 'body', 'head', 'meta', 'title', 'script']
+
+        if self.options.delimiter == 'spaces':
+            delimiter = '  '
+        else:
+            delimiter = '\t'
+
+        for action, elem in html:
+            if (action == 'start'):
+                identifier = self.identify_ruleset(elem)
+
+                if elem.tag not in ignore_tags:
+                    level += 1
+                    css += delimiter * level + identifier + ' {\n'
+                    if not self.options.clean_mode:
+                        css += delimiter + delimiter * level + '/* enter your CSS here... */\n'
+            else:
+                if elem.tag not in ignore_tags:
+                    css += delimiter * level + '}\n'
+                    level -= 1
+
+        return css.strip()
+
+    def run(self):
+        for file in self.args:
+            f = open(file, "r")
+            content = f.read()
+            print self.parse(content)
+
+if __name__ == '__main__':
+    importer = Html2Less()
+    importer.run()
